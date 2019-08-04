6cf326abbabd381ea77cf93e284409faab48b922
Sergey Brazgun <sampad777@gmail.com>
2019-08-04 13:42:39 +0300

    First commit

---
+++ src/Parser.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace brazgum\parser;
+
+class Parser implements ParserInterface
+{
+
+    /**
+     * @param string $url
+     * @param string $tag
+     * @return array
+     */
+    public function process(string $tag, string $url): array
+    {
+
+        $htmlPage = file_get_contents($url);
+
+        if ($htmlPage === false) {
+            return ['Invalid URL'];
+        }
+        
+        // Regular expression
+
+        preg_match_all('/<' . $tag . '.*?>(.*?)<\/' . $tag . '>/s', $htmlPage, $strings);
+
+        if (empty($strings[1])) {
+            return ['There are no such tags on the page'];
+        }
+
+        return $strings[1];
+    }
+    
+    public function test()
+    {
+        // feature
+    }
+
+}
